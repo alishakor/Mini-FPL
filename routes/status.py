6d82f23d56ccad5747f8172f62598482705a12fb
alishakor <aliuokeowo@gmail.com>
2023-11-03 11:13:11 +0100

    dev

--- routes/status.py
+++ routes/status.py
@@ -4,8 +4,11 @@ Handling the status route
 """
 from models.basemodel import app
 from flask import render_template
+from flask_login import current_user, login_required
 
 
 @app.route('/status', methods=['GET', 'POST'], strict_slashes=False)
+@login_required
 def status():
-    return render_template('status.html')
+    username = current_user.username
+    return render_template('status.html', username=username)
